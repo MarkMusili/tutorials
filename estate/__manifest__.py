0ec876f6348f644d8f74cf8930093f10e3eb05cd
MarkMusili <musiliyrn@gmail.com>
2024-07-04 01:10:32 +0300

    Custom module "empty shell"

---
+++ estate/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': "estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Mark Musili",
+    'category': 'Category',
+    'description': """A real-estate module to help sellers sell their property.""",
+}
