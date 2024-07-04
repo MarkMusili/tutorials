83dcc035d541c754c20ded7f2b112a4d485bee20
MarkMusili <musiliyrn@gmail.com>
2024-07-04 14:33:15 +0300

    Basic module implimentation

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,4 +6,6 @@
     'category': 'Category',
     'description': """A real-estate module to help sellers sell their property.""",
     'application': True,
+    'installable': True,
+    'licence': 'LGPL-3',
 }
---
+++ estate/models/property.py
@@ -0,0 +1,6 @@
+from odoo import models, fields
+
+class Property(models.Model):
+    """Model for properties"""
+    _name = 'estate.property'
+
