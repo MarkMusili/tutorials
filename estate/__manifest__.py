76b94538cada7d5781e0db2f24419f247cc8d1a0
MarkMusili <musiliyrn@gmail.com>
2024-07-11 16:31:23 +0300

    Views and menus for estate model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,6 +9,8 @@
     'installable': True,
     'licence': 'LGPL-3',
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ]
 }
